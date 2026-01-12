53363b449ebe0f3ac4241fae939af1b915894573
Jonathan Horner <jonathan.horner@protonmail.com>
2026-01-12 19:55:59 +0000

    Reverted back to original CUDA sample code.

--- include/helper_functions.h
+++ include/helper_functions.h
@@ -35,8 +35,9 @@
 #endif
 
 // includes, project
+#include "exception.h"
+
 #include <assert.h>
-#include <exception.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -48,12 +49,12 @@
 #include <vector>
 
 // includes, timer, string parsing, image helpers
-#include <helper_image.h>  // helper functions for image compare, dump, data comparisons
-#include <helper_string.h>  // helper functions for string parsing
-#include <helper_timer.h>   // helper functions for timers
+#include "helper_image.h"     // helper functions for image compare, dump, data comparisons
+#include "helper_string.h"    // helper functions for string parsing
+#include "helper_timer.h"     // helper functions for timers
 
 #ifndef EXIT_WAIVED
 #define EXIT_WAIVED 2
 #endif
 
-#endif  // COMMON_HELPER_FUNCTIONS_H_
+#endif    // COMMON_HELPER_FUNCTIONS_H_
